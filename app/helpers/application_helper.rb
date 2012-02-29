f65bd1462fb0a22056632552d5382e326d4e6fa6
sv <sisop11@gmail.com>
2012-02-29 14:38:36 +0400

    HZ

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def logo
+    logo_file = "logo.png"
+    params = { :alt => "Sample App", :class => "round" }
+    image_tag(logo_file, params)
+  end 
+
   # Return a title on a per-page basis.
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
